71a17318b70aa797fffff1c86af60aa9e98d5393
rogersb11 <brettrogers11@gmail.com>
2016-05-06 15:11:30 -0400

    [SQUASHED]Dashboard Font style and size

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5009,6 +5009,24 @@ public final class Settings {
         public static final String HEADS_UP_SHOW_STATUS_BUTTON = "heads_up_show_status_button";
   
         /**
+         * Settings Dash Font Style
+         * @hide
+         */
+        public static final String DASHBOARD_FONT_STYLE = "dashboard_font_style";
+ 
+        /**
+         * Dashboard title text size
+         * @hide
+         */
+        public static final String SETTINGS_TITLE_TEXT_SIZE = "settings_title_text_size";
+
+        /**
+         * Dashboard category text size
+         * @hide
+         */
+        public static final String SETTINGS_CATEGORY_TEXT_SIZE = "settings_category_text_size";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
